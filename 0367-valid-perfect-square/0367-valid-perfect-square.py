fba6866f2ab285a9cbd66aeaa9ea71f0cc717437
Sathwika <139571325+sathwikabethu@users.noreply.github.com>
2026-07-12 22:06:39 +0530

    Time: 8 ms (12.75%), Space: 19.2 MB (88.71%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        i=0
+        while i*i<=num:
+            if i*i==num:
+                return True
+            i+=1
+        else:
+            return False
+        
